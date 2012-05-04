be80fbd03a5e4a033615a778822e969849be2b8d
Brett <brett@socketstudio.com>
2012-05-04 13:50:34 -0400

    removed specific foreground/app_screen ids; genericized place on screen

--- js/script.js
+++ js/script.js
@@ -14,62 +14,61 @@
 $(function(){
   //--VARIABLES
   var speed=0.64;
-  var height_diff=-190; //negative number is higher up the page
-  var bottom_thresh=3855;
-  var iphone_top, $iphone,$ff1,$ff2,$ff3,$ff4,$ff5,$ib1,$ib2,$ib3,$ib4,$tweets,$onscreen;
+  var height_diff=-95; //negative number is higher up the page
+  var iphone_top, $iphone,$tweets,$onscreen,$feature_foregrounds,$app_screens,bottom_thresh;
   var count=5;
   var wait=1000;
   $onscreen=$('#onscreen');
   var current_scroll=window.scrollY||document.documentElement.scrollTop;
 	$onscreen.css("top",-current_scroll+"px");
-	//feature foregrounds
-	$ff1=$('#feature_foreground_1');
-	$ff2=$('#feature_foreground_2');
-	$ff3=$('#feature_foreground_3');
-	$ff4=$('#feature_foreground_4');
-	$ff5=$('#feature_foreground_5');
-	//iphone foregrounds
-	$if1=$('#app_screen_1');
-	$if2=$('#app_screen_2');
-	$if3=$('#app_screen_3');
-	$if4=$('#app_screen_4');
-	$iphone=$('#iphone_area');
+	$iphone=$('#iphone');
 	$tweets=$('#tweets');
+	$feature_foregrounds=$(".feature_foreground");
+	$app_screens=$(".app_screen");
+	bottom_thresh = $($feature_foregrounds[$feature_foregrounds.length-1]).offset().top;
 	
 	
 	function scrolling(){
-    var screen_pos;
-    current_scroll=window.scrollY||document.documentElement.scrollTop;
-    
-  
-      if(current_scroll<bottom_thresh){
-        screen_pos=current_scroll*speed;
-        $onscreen.css("top",-screen_pos+"px");
-        $iphone.css({"position":"","top":""});
-        iphone_top=null;
-    }//if within the threshold
-    else{
-      if(iphone_top===null)
-        iphone_top=bottom_thresh;//$("#iphone").offset().top;
-        screen_pos=(bottom_thresh)*speed;
-        $onscreen.css("top",-screen_pos+"px");
-        $iphone.css({"position":"absolute","top":iphone_top+"px"});
+	    var screen_pos;
+	    current_scroll=window.scrollY||document.documentElement.scrollTop;
+	    
+	  
+	      if(current_scroll<bottom_thresh){
+	        screen_pos=current_scroll*speed;
+	        $onscreen.css("top",-screen_pos+"px");
+	        $iphone.css({"position":"","top":""});
+	        iphone_top=null;
+	    }//if within the threshold
+	    else{
+	      if(iphone_top===null)
+	        iphone_top=bottom_thresh;//$("#iphone").offset().top;
+	        screen_pos=(bottom_thresh)*speed;
+	        $onscreen.css("top",-screen_pos+"px");
+	        $iphone.css({"position":"absolute","top":iphone_top+"px"});
 
-    }//else
+	    }//else
   }// function scrolling
 	
 	function placeOnScreen(){ 
     
     if(window.scrollY>=bottom_thresh) scrolling();
-		var background_top1=$ff1.offset().top;
-		background_top1+=height_diff;
-		background_top1*=speed;
-		$if1.css("top",background_top1+"px");
+    
+    $feature_foregrounds.each(function(i){
+    	var top=$(this).offset().top
+    	top+=height_diff;
+    	top*=speed;
+    	$($app_screens[i+1]).css("top",top+"px");
+    })
+    /*
+	var background_top1=$ff1.offset().top;
+	background_top1+=height_diff;
+	background_top1*=speed;
+	$if1.css("top",background_top1+"px");
   
     var background_top2=$ff2.offset().top;
-		background_top2+=height_diff;
-		background_top2*=speed;
-		$if2.css("top",background_top2+"px");
+	background_top2+=height_diff;
+	background_top2*=speed;
+	$if2.css("top",background_top2+"px");
 	
 		var background_top3=$ff3.offset().top;
 		background_top3+=height_diff;
@@ -88,7 +87,7 @@ $(function(){
 		background_top5*=speed;
 		$tweets.css("top",background_top5+"px");
 	
-	
+		*/
 
   	
 		}//function placeOnScreen
